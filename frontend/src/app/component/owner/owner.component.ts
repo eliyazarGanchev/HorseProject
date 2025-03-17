054db1ee1ee635f276dc0453601b1f94c2f07806
11 <e12026921@student.tuwien.ac.at>
2025-03-17 18:03:58 +0200

    [US 8] Implement all created Owners to be visible
    - Implement owner.component.html, owner.component.scss and owner.component.ts

---
+++ frontend/src/app/component/owner/owner.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { OwnerService } from 'src/app/service/owner.service';
+import { Owner } from 'src/app/dto/owner';
+
+@Component({
+  selector: 'app-owner',
+  templateUrl: './owner.component.html',
+  imports: [
+    RouterLink,
+    FormsModule,
+  ],
+  standalone: true,
+  styleUrls: ['./owner.component.scss']
+})
+export class OwnerComponent implements OnInit {
+  owners: Owner[] = [];
+  bannerError: string | null = null;
+  ownerForDeletion: Owner | undefined;
+
+  constructor(
+    private service: OwnerService,
+    private notification: ToastrService,
+  ) { }
+
+  ngOnInit(): void {
+    this.reloadOwners();
+  }
+
+  reloadOwners() {
+    this.service.getAll()
+      .subscribe({
+        next: data => {
+          this.owners = data;
+          this.bannerError = null;
+        },
+        error: error => {
+          console.error('Error fetching owners', error);
+          this.bannerError = 'Could not fetch owners: ' + error.message;
+          const errorMessage = error.status === 0
+            ? 'Is the backend up?'
+            : error.message.message;
+          this.notification.error(errorMessage, 'Could Not Fetch Owners');
+        }
+      });
+  }
+
+}
